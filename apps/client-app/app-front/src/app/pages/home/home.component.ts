60ce601805443c1989170b6254cbb3d4abedc987
sekvi99 <filmatkozlik@gmail.com>
2023-09-05 20:04:26 +0200

    Changed existing forms for angular material and form builder

--- apps/client-app/app-front/src/app/pages/home/home.component.ts
+++ apps/client-app/app-front/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { YoutubeStatsService } from 'src/app/services/youtube.stats-service';
 import { YouTubeStatsDto } from 'src/app/models/yt-stats/yt.stats-dto';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,13 +13,22 @@ export class HomeComponent {
 
   videoUrl: string = '';
   stats!: YouTubeStatsDto;
+  videoForm!: FormGroup;
 
-  constructor(private youTubeStatsService: YoutubeStatsService) { }
+  constructor(
+    private youTubeStatsService: YoutubeStatsService,
+    private formBuilder: FormBuilder // Inject the FormBuilder
+  ) {
+    this.videoForm = this.formBuilder.group({
+      videoUrl: ['', [Validators.required]]
+    });
+  }
 
-  onVideoSubmit(): void{
-    console.log(this.videoUrl);
-    if (this.videoUrl.length > 0) {
-      this.youTubeStatsService.getStats(this.videoUrl).subscribe(
+  onVideoSubmit(): void {
+    console.log(this.videoForm.value.videoUrl);
+
+    if (this.videoForm.valid) {
+      this.youTubeStatsService.getStats(this.videoForm.value.videoUrl).subscribe(
         res => {
           this.stats = res;
           console.log(this.stats);
@@ -27,9 +37,8 @@ export class HomeComponent {
           console.log('Error', error);
         }
       );
-
-    }else {
-      console.log(`Can not process this url: ${this.videoUrl}`);
+    } else {
+      console.log('Form is invalid.');
     }
   }
 
--- apps/client-app/app-front/src/app/pages/login/login.component.ts
+++ apps/client-app/app-front/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Import Validators and FormGroup
 import { UserService } from 'src/app/services/user-service';
 import { Router } from '@angular/router';
 import { AuthEventService } from 'src/events/auth-event-service';
@@ -10,31 +11,52 @@ import { AuthenticationDto } from 'src/app/models/auth/authentication-dto';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   isErrorInLogin: boolean = false;
   authRequest: AuthenticationDto = {
     username: '',
     password: ''
+  };
+
+  loginForm: FormGroup; // Use FormGroup
+
+  constructor(
+    private userService: UserService,
+    private authEventService: AuthEventService,
+    private router: Router,
+    private formBuilder: FormBuilder // Inject the FormBuilder
+  ) {
+    // Initialize the login form with validators
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
   }
 
-  constructor(private userService: UserService, private authEventService: AuthEventService, private router: Router) { }
-
   ngOnInit(): void {
-      this.authEventService.logout();
+    this.authEventService.logout();
   }
 
   public onLogin(): void {
-    this.userService.authenticate(this.authRequest).subscribe(response => {
-      if (response) {
-        this.authEventService.login();
-        this.router.navigateByUrl('/home');
-      } else {
-        this.isErrorInLogin = true;
-      }
-    }, error => {
-      this.isErrorInLogin = true;
-    });
+    // Check if the form is valid before submitting
+    if (this.loginForm.valid) {
+      // Assign form values to the authRequest object
+      this.authRequest['username'] = this.loginForm.get('username')?.value;
+      this.authRequest['password'] = this.loginForm.get('password')?.value;
+
+      this.userService.authenticate(this.authRequest).subscribe(
+        (response) => {
+          if (response) {
+            this.authEventService.login();
+            this.router.navigateByUrl('/home');
+          } else {
+            this.isErrorInLogin = true;
+          }
+        },
+        (error) => {
+          this.isErrorInLogin = true;
+        }
+      );
+    }
   }
   
-
 }
